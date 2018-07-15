01e02c1c66a18f53d1e91acacf829bc82c0527d9
valt2017 <valt@inmail.sk>
2018-07-15 14:37:47 +0200

    mouse events added

--- SDL/Source.cpp
+++ SDL/Source.cpp
@@ -10,38 +10,74 @@
 const int BoardSize = 8;
 int board[BoardSize][BoardSize] =
 { -1,-2,-3,-4,-5,-3,-2,-1,
--6,-6,-6,-6,-6,-6,-6,-6,
-0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0,
-0, 0, 0, 0, 0, 0, 0, 0,
-6, 6, 6, 6, 6, 6, 6, 6,
-1, 2, 3, 4, 5, 3, 2, 1 };
+  -6,-6,-6,-6,-6,-6,-6,-6,
+   0, 0, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 0, 0, 0,
+   0, 0, 0, 0, 0, 0, 0, 0,
+   6, 6, 6, 6, 6, 6, 6, 6,
+   1, 2, 3, 4, 5, 3, 2, 1 };
 
 //figures coordinates
 const int BoardOffset = 2;
 std::map<int, SDL_Rect> Figures;
 const int FigureSize = 56;
+std::map<const char, SDL_Texture *> Textures;
+std::map<const char, std::string> Bitmaps;
+const int posX = 100, posY = 100;
+const int sizeX = 454, sizeY = 454;
+std::string moves = "";
+
+std::string toChessNote(SDL_Point & position)
+{ // www.asciitable.com :-)
+	std::string s = "";
+	s += char(position.x / FigureSize + 'a');
+	s += char(7 - position.y / FigureSize + '1');
+	return s;
+}
+
+SDL_Point toBoard(char a, char b)
+{
+	SDL_Point position;
+	position.x = int(a) - 97;
+	position.y = 7 - int(b) + 49;
+	return position;
+}
+
+void performMove(std::string str)
+{
+	SDL_Point oldPos = toBoard(str[0], str[1]);
+	SDL_Point newPos = toBoard(str[2], str[3]);
+	std::cout << "X:" << oldPos.x << "Y:" << oldPos.y << " " << board[oldPos.y][oldPos.x] << std::endl;
+	std::cout << "X:" << newPos.x << "Y:" << newPos.y << " " << board[newPos.y][newPos.x] << std::endl;
+	if (board[oldPos.y][oldPos.x] != 0 && board[newPos.y][newPos.x] == 0) {
+		board[newPos.y][newPos.x] = board[oldPos.y][oldPos.x];
+		board[oldPos.y][oldPos.x] = 0;
+	}
+
+/*
+	//castling       //if the king didn't move
+	if (str == "e1g1") if (position.find("e1") == -1) move("h1f1");
+	if (str == "e8g8") if (position.find("e8") == -1) move("h8f8");
+	if (str == "e1c1") if (position.find("e1") == -1) move("a1d1");
+	if (str == "e8c8") if (position.find("e8") == -1) move("a8d8");
+	*/
+}
 
 
 int main(int argc, char ** argv) {
 	SDL_Window *win = NULL;
 	SDL_Renderer *renderer = NULL;
-	std::map<const char, SDL_Texture *> Textures;
-	std::map<const char, std::string> Bitmaps;
-	const int posX = 100, posY = 100;
-	const int sizeX = 454, sizeY = 454;
 	// Error checks
-	std::cout << "SDL init start" << std::endl;
-	
+	std::cout << "SDL_Init\n";
 	SDL_Init(SDL_INIT_VIDEO);
 	win = SDL_CreateWindow("SDL & Stockfish", posX, posY, sizeX, sizeY, 0);
 	if (win == NULL) {
-		std::cout << "SDL_CreateWindow error"<<std::endl;
+		std::cout << "SDL_CreateWindow error\n";
 	}
 	renderer = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
 	if (renderer == NULL) {
-		std::cout << "SDL_CreateRenderer error" << std::endl;
+		std::cout << "SDL_CreateRenderer error\n";
 	}
 	// Load bitmaps
 	Bitmaps[0] = "img/board0.bmp";
@@ -50,14 +86,14 @@ int main(int argc, char ** argv) {
 	for (auto bitmap : Bitmaps) {
 		SDL_Surface * bitmapSurface = SDL_LoadBMP(bitmap.second.c_str());
 		//transparent color -> red
-		bitmapSurface->format->Amask = 0xFF000000;
-		bitmapSurface->format->Ashift = 24;
 		SDL_SetColorKey(bitmapSurface, SDL_TRUE, SDL_MapRGB(bitmapSurface->format, 255, 0, 0));
 		// create texture with modified color (transparent)
 		Textures[bitmap.first] = SDL_CreateTextureFromSurface(renderer, bitmapSurface);
 		if (Textures[bitmap.first] == NULL) {
-			std::cout << bitmap.first <<" SDL_CreateTextureFromSurface error" << std::endl;
+			std::cout << bitmap.second.c_str() <<" SDL_CreateTextureFromSurface error\n";
 		}
+		else
+			std::cout << bitmap.second.c_str() << " SDL_CreateTextureFromSurface OK\n";
 		SDL_FreeSurface(bitmapSurface);
 	}
 	// Coordinates for figures from bitmap
@@ -71,24 +107,29 @@ int main(int argc, char ** argv) {
 			if (e.type == SDL_QUIT) {
 				break;
 			}
-			/* Look for a keypress */
-			if (e.type == SDL_KEYDOWN) {
-				switch (e.key.keysym.sym) {
-				case SDLK_LEFT:
-					break;
-				case SDLK_RIGHT:
-					break;
-				case SDLK_UP:
-					break;
-				case SDLK_DOWN:
-					break;
-				case SDLK_RETURN:
-					break;
-				case SDLK_SPACE:
-					break;
-				default:
-					std::cout << "Key not supported" << std::endl;
-					break;
+			/* Mouse button down */
+			if (e.type == SDL_MOUSEBUTTONDOWN) {
+				SDL_Point mousePos;
+				mousePos.x = e.motion.x;
+				mousePos.y = e.motion.y;
+				if (e.button.button == SDL_BUTTON_LEFT){
+					moves = toChessNote(mousePos).c_str();
+				}
+			}
+			/* Mouse button up */
+			if (e.type == SDL_MOUSEBUTTONUP) {
+				SDL_Point mousePos;
+				mousePos.x = e.motion.x;
+				mousePos.y = e.motion.y;
+				if (e.button.button == SDL_BUTTON_LEFT) {
+					std::string newPos = toChessNote(mousePos);
+					if (moves != newPos) {
+						moves += newPos;
+						std::cout << "Move to execute:" << moves.c_str() << std::endl;
+						performMove(moves);
+					}
+					else
+						moves = "";
 				}
 			}
 		}
@@ -118,7 +159,6 @@ int main(int argc, char ** argv) {
 						SDL_RenderCopy(renderer, texture->second, &it->second, &DestR);
 					}
 				}
-
 		}
 		SDL_RenderPresent(renderer);
 	}
@@ -128,5 +168,6 @@ int main(int argc, char ** argv) {
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(win);
 	SDL_Quit();
+	std::cout << "SDL_Quit\n";
 	return 0;
 }
